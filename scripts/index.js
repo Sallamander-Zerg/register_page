4e0e46cb5d7bc429095c443195a359e7a0df1b0c
SALL <62800760+Sallamander-Zerg@users.noreply.github.com>
2023-03-09 22:25:43 +0700

    fix error

--- scripts/index.js
+++ scripts/index.js
@@ -3,34 +3,21 @@ let password = document.getElementById('field4');
 let password2 = document.getElementById('field5');
 let date = document.getElementById('field6');
 let form = document.querySelector('.popup__form');
-let name = document.getElementById('field1');
-let surname = document.getElementById('field2');
+let errorP = document.querySelector('#errorP');
+let errorD = document.querySelector('#errorD');
 function confermform(evt){
     evt.preventDefault(); 
     let bornedate = new Date(date.value);
     let nawday = new Date()
-    let nameS = name.value;
-    let surnameS = surname.value;
-    let passwordl = password.value;
-    let numdersinName = /^[а-яА-Я]+$/.test(nameS);
-    let numdersinSurname = /^[а-яА-Я]+$/.test(surnameS);
-    let testPassword = /(?=.*[A-ZА-Я])/.test(passwordl);
-    let testPassword2 = /(?=.*[0-9])/.test(passwordl);
     age=nawday.getFullYear()-bornedate.getFullYear();
-    if((numdersinName === false)||(numdersinSurname === false)){
-        alert("в имении и фамилии не должы содержать постароние символы")
-        return 0;
-     }
-     if((testPassword2 === false)||(testPassword === false)){
-      alert("пароль должен содержать хотябы одну большую букву и хотябы одну цыфру")
-      return 0;
-    }
     if(password.value !== password2.value){
-       alert("пароли не совпадают")
+        errorP.textContent = "пароли не совпадают";
+        errorP.classList.add('.popup__field-error_active');
        return 0
     }
     if( age < 18){
-        alert("вам ещё рано заходить на даный сайт")
+        errorD.textContent = "вам нет 18";
+        errorD.classList.add('.popup__field-error_active');
         return 0
      }
     form.reset()
